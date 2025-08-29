c9889a72cfdd70b9afeac41e6b3ee6209b1e07e8
Moshe <moishi67@gmail.com>
2025-08-29 17:22:28 +0300

    fix exception throwing

--- HestiaCP/HestiaCP.php
+++ HestiaCP/HestiaCP.php
@@ -134,7 +134,7 @@ class HestiaCP extends Server
         $str = '';
         $max = mb_strlen($keyspace, '8bit') - 1;
         if ($max < 1) {
-            throw new Exception('$keyspace must be at least two characters long');
+            throw new \Exception('$keyspace must be at least two characters long');
         }
         for ($i = 0; $i < $length; ++$i) {
             $str .= $keyspace[random_int(0, $max)];
